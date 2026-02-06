7317ae8a1e13d17b5a8c759ae683871bccbfdcb7
Xuliang (Harry) Sun <32334165+xuliangs@users.noreply.github.com>
2026-02-05 18:03:59 -0800

    revert relation comment (#7915)
    GitOrigin-RevId: cfd1aa27290d7a5eb6e7b2d31c47fdfffa8696ec

--- crates/dbt-loader/src/dbt_macro_assets/dbt-bigquery/macros/adapters.sql
+++ crates/dbt-loader/src/dbt_macro_assets/dbt-bigquery/macros/adapters.sql
@@ -118,7 +118,11 @@
 {% endmacro %}
 
 {% macro bigquery__alter_relation_comment(relation, relation_comment) -%}
-  {% do adapter.update_table_description(relation.database, relation.schema, relation.identifier, relation_comment) %}
+  {#
+    DIVERGENCE: FIXME: Fusion needs to implement the adapter.update_table_description method using googleapi.
+    otherwise the existing logic fails on View models.
+    {% do adapter.update_table_description(relation.database, relation.schema, relation.identifier, relation_comment) %}
+  #}
 {% endmacro %}
 
 {% macro bigquery__alter_column_comment(relation, column_dict) -%}
